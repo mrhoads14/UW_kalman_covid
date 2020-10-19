82dd9752ddbff0313663aed5376f39bdf8d5ac69
Mike R <21145750+mrhoads14@users.noreply.github.com>
2020-10-18 21:40:49 -0700

    refactor to be date specific, add epsilon to H, adjust for validation data

--- kalman.py
+++ kalman.py
@@ -12,6 +12,12 @@ import seiir_compartmental
 """
 Runs an extended Kalman filter on Prof Flaxman's SEIIR predictions and
 measurement data for New York State from mid-April to 7 July 2020.
+
+S - Susceptible
+E - Exposed
+I1 - Presymptomatic
+I2 - Symptomatic
+R - Recovered
 """
 
 # https://github.com/ihmeuw/vivarium_uw_covid/blob/master/src/vivarium_uw_covid/components/seiir_compartmental.py#L11-L67
@@ -31,38 +37,54 @@ def main():
         'sigma': 0.266635,
         'theta': 6.000000
         }
+
+    # set initial values for Kalman filter parameters
     P_mult = 1
     Q_mult = 1
 
-    Rn_mult = 5*10**-4
-    Rn_22 = 100
-    Rn_32 = 100
+    # Rn is the R noise matrix; it remains constant thru the stepping of the
+    # Kalman filter
+    # prior to experiments, Rn_mult = 5*10**-4
+    Rn_mult = 5*10**-8
 
-    Rn = Rn_mult * np.array([[1, 0, 0, 0, 0],
-                             [0, 1, 0, 0, 0],
-                             [0, 0, Rn_22, 0, 0],
-                             [0, 0, Rn_32, 1, 0],
-                             [0, 0, 0, 0, 1]])
+    Rn_22 = 100
+    Rn_32 = 1
+
+    Rn_23 = 1
+    Rn_33 = 50
+
+    Rn = Rn_mult * np.array([[0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0],
+                             [0, 0, Rn_22, Rn_23, 0],
+                             [0, 0, Rn_32, Rn_33, 0],
+                             [0, 0, 0, 0, 0]])
     Q = Q_mult * np.eye(5)
     P = P_mult * np.eye(5)
+
+    # colors used in plots
     purple = '#33016F'
     gold = '#9E7A27'
     gray = '#797979'
 
     # generate data
-    seiir, beta, fb_data, case_data = get_data_sets(data_sets.STATES[the_state],
-                                                    fips=the_county)
+    seiir, fb_data, fb_data_val, case_data = get_data_sets(
+        data_sets.STATES[the_state], fips=the_county)
 
     county_pop, state_pop = data_sets.get_pops(the_county)
     b = county_pop / state_pop
 
     # calculate moving averages on the fb and case data
     prop_ma7, case_ma7_all = calc_ma7(fb_data, case_data)
+    
+
+
+
+    
     # ensure the case rate data is the same date range as the prop data
     case_ma7 = case_ma7_all['2020-04-12':'2020-07-07']
 
     # get starting compartment values for the state level
-    x_hat_state_k0, beta_k0 = get_predicts_prior(0, seiir, beta)
+    x_hat_state_k0, beta_k0 = get_predicts_prior(K0, seiir)
 
     # approximate rho values
     # I2_county = b * I2
@@ -71,7 +93,7 @@ def main():
 
     x_hat_k0 = b * x_hat_state_k0
     I2_county = x_hat_k0[3, 0]
-    rho1 = 1000 * prop_ma7[0] / I2_county
+    rho1 = 1000 * prop_ma7.loc['2020-04-12'] / I2_county
     rho2 = case_ma7_all.loc['2020-04-12'] / I2_county
 
     # create empty dictionaries to hold the estimated values
@@ -79,14 +101,15 @@ def main():
     case_est = {}
     seiir_pred = {}
 
-    k = 0
-    while K0 + datetime.timedelta(days=k) <= case_ma7.index[-1]:
+    # each iteration of the while loop is a step for the Kalman filter
+    d = K0
+    while d <= case_ma7.index[-1]:
         # each cycle of the while loop executes a step
 
         # get state level compartments
-        x_hat_state_k, beta_k = get_predicts_prior(k, seiir, beta)
+        x_hat_state_k, beta_k = get_predicts_prior(d, seiir)
 
-        # step the state level compartments one step forward (7 days)
+        # step the state level compartments 7 days forward
         x_hat_state_k1 = step_seiir(x_hat_state_k, constants, beta_k)
 
         # convert the state level compartments to county level values
@@ -96,8 +119,8 @@ def main():
         # get measurements
         z_k = np.array([[0],
                         [0],
-                        [prop_ma7[k]],
-                        [case_ma7[k]],
+                        [prop_ma7.loc[d]],
+                        [case_ma7.loc[d]],
                         [0]])
 
         # predict step
@@ -105,29 +128,37 @@ def main():
         print('step -----------')
         print('P:')
         print(P)
+
         # update step
         x_hat_post, P_post = update_step(x_hat_k, x_hat_k1, P, Rn,
                                          rho1, rho2, z_k)
         print('P_post:')
         print(P_post)
+
         # store estimated values for proportion and case rate
-        indexDate = K0 + datetime.timedelta(days=k+7)
+        indexDate = d + datetime.timedelta(days=7)
         prop_est[indexDate] = rho1 * x_hat_post[3, 0]
         case_est[indexDate] = rho2 * x_hat_post[3, 0]
         seiir_pred[indexDate] = b * x_hat_k1[3, 0]
 
-        # update the P and k
+        # update the P and d
         P = P_post
-        k += 1
+        d += datetime.timedelta(days=1)
 
     # create pandas series of the estimated case rate
     predicted_case = pd.Series(case_est)
-
     predicted_seiir_prior = pd.Series(seiir_pred)
 
+    # compute squared error for the overlapping time period
     overlap = pd.date_range(start='2020-04-19', end='2020-07-07')
     overlap_dt = [x.to_pydatetime().date() for x in overlap]
+
+    # error between the measured case rate (smoothed) and the
+    # SEIIR model scaled to the county (without any Kalman adjustment)
     seiir_sq_err = 0
+    
+    # error between the measured case rate (smoothed) and the predicted
+    # output of the Kalman filter
     kalman_sq_err = 0
 
     for day in overlap_dt:
@@ -199,23 +230,19 @@ def main():
     plt.ylabel('Percentage of Positive Symptom Response')
     plt.legend(loc='upper right')
 
-    #ax.set_title('Albany County measured case rate & symptom prop vs. 7 day prediction using Kalman filter estimate')
-    #fig.suptitle('R = ' + str(Rn_mult) + ' * Identity Matrix')
-
 
     plt.show()
 
 
 # functions to support the Kalman filtering
-def get_predicts_prior(k, seiir, beta, k0=K0):
-    day = k0 + datetime.timedelta(days=k)
+def get_predicts_prior(day, seiir):
     x_hat = np.array([[seiir['S'].loc[day]],
                       [seiir['E'].loc[day]],
                       [seiir['I1'].loc[day]],
                       [seiir['I2'].loc[day]],
                       [seiir['R'].loc[day]]])
 
-    beta_k = beta.loc[day]
+    beta_k = seiir['beta_pred'].loc[day]
 
     return x_hat, beta_k
 
@@ -295,31 +322,38 @@ def predict_step(x_hat_k1_prior, P, Q, beta_k, constants):
                               part_f_R], axis=1)
 
     # 5x5
+    # P_k1_prior = f_jacob * P * f_jacob^T + Q
     P_k1_prior = np.matmul(np.matmul(f_jacob, P), np.transpose(f_jacob)) + Q
     return P_k1_prior
 
 
 def update_step(x_hat, x_hat_k1, P_k1, Rn, rho1, rho2, z_k):
     # 5x5
-    H_new = np.array([[0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0],
-                      [0, 0, 0, rho1, 0],
-                      [0, 0, 0, rho2, 0],
-                      [0, 0, 0, 0, 0]])
+    ep = 10**-10
+    H = np.array([[ep, 0, 0, 0, 0],
+                  [0, ep, 0, 0, 0],
+                  [0, 0, ep, rho1, 0],
+                  [0, 0, 0, rho2, 0],
+                  [0, 0, 0, 0, ep]])
 
-    # Si = H_new * P_k1 * H_new^T + Rn
-    Si = np.matmul(np.matmul(H_new, P_k1), np.transpose(H_new)) + Rn
+    # Si = H * P_k1 * H^T + Rn
+    Si = np.matmul(np.matmul(H, P_k1), np.transpose(H)) + Rn
 
-    # K_new = P_k1 * H_new^T * Si^(-1)
-    K_new = np.matmul(np.matmul(P_k1, np.transpose(H_new)), np.linalg.inv(Si))
-    y_new = np.matmul(H_new, x_hat)
+    # K_new = P_k1 * H^T * Si^(-1)
+    K_new = np.matmul(np.matmul(P_k1, np.transpose(H)), np.linalg.inv(Si))
+    y_new = np.matmul(H, x_hat)
 
     # 5x1
     diff = z_k - y_new
 
     x_hat_k1_post = x_hat_k1 + np.matmul(K_new, diff)
 
-    P_k1_post = P_k1 - np.matmul(np.matmul(K_new, Si), np.transpose(K_new))
+    # P_k1_post = P_k1 - np.matmul(np.matmul(K_new, Si), np.transpose(K_new))
+
+    # joseph formulation
+    joe2 = np.matmul(np.matmul(K_new, Rn), np.transpose(K_new))
+    joe1 = np.eye(5) - np.matmul(K_new, H)
+    P_k1_post = np.matmul(np.matmul(joe1, P_k1), np.transpose(joe1)) - joe2
 
     return x_hat_k1_post, P_k1_post
 
@@ -356,22 +390,29 @@ def get_data_sets(state, fips=None):
     seiir = pd.read_csv(r'data/seiir_compartments_post-hoc_ny_state.csv', header=0,
                         index_col='date', parse_dates=True)
     # beta time series provided by Prof Flaxman:
-    beta = pd.read_csv(r'data/beta_t_ny.csv', header=0,
-                       index_col='date', parse_dates=True, squeeze=True)
+    #beta = pd.read_csv(r'data/beta_t_ny.csv', header=0,
+    #                   index_col='date', parse_dates=True, squeeze=True)
 
     fb_data = data_sets.create_symptom_df()
+    fb_data_val = data_sets.create_symptom_df(valid=True)
 
     if fips is None:
         case_data = data_sets.create_case_df_state()
         case_data_geo = case_data.loc[state]['case_rate'].copy()
         fb_data_geo = fb_data.loc[state].groupby('date').sum().copy()
+        fb_data_val_geo = fb_data_val.loc[state].groupby('date').sum().copy()
 
     else:
         case_data = data_sets.create_case_df_county()
         case_data_geo = case_data.loc[fips]['case_rate'].copy()
         fb_data_geo = fb_data.loc[(slice(None), fips), :].copy()
+        fb_data_val_geo = fb_data_val.loc[(slice(None), fips), :].copy()
 
-    return seiir, beta, fb_data_geo, case_data_geo
+        # collapse down to a single index column (date)
+        fb_data_geo = fb_data_geo.mean(level='date')
+        fb_data_val_geo = fb_data_val_geo.mean(level='date')
+
+    return seiir, fb_data_geo, fb_data_val_geo, case_data_geo
 
 
 def calc_ma7(fb_data, case_data):
